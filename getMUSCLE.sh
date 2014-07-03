98a817fccb1017204ad7eb5765e83591c4d2fb8d
Augustin Cavalier <waddlesplash@gmail.com>
2014-07-03 17:05:48 +0000

    Update MUSCLE version.

--- getMUSCLE.sh
+++ getMUSCLE.sh
@@ -1,6 +1,6 @@
 # Gets a copy of MUSCLE from the server, and sets it up
 # so that BeShare is ready to be built!
-MUSCLE_VERSION=6.02
+MUSCLE_VERSION=6.05
 wget https://public.msli.com/lcs/muscle/muscle$MUSCLE_VERSION.zip --no-check-certificate
 unzip muscle$MUSCLE_VERSION.zip -d .
 rm muscle$MUSCLE_VERSION.zip
